ad8554ea38b1816400ff2fcad83b14e6947c1728
Irzum Bin Mansoor Jafri <irzumbm@gmail.com>
2024-03-10 19:26:34 +0200

    save_load map script

--- MappingModule/Assets/MapManager.cs
+++ MappingModule/Assets/MapManager.cs
@@ -8,16 +8,29 @@ using UnityEngine.UI;
 using TMPro;
 using System;
 
-class MapManager : Singleton {
+class MapManager : Singleton
+{
     List<Map> maps;
-    
-    void LoadMaps(){
+
+    void LoadMaps()
+    {
+        string filePath = Path.Combine(Application.persistentDataPath, "maps.json");
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            maps = JsonUtility.FromJson<List<Map>>(json);
+            Debug.Log(maps);
+        }
 
     }
 
     // serialize maps into json
     // use Application.persistentDataPath + maps.data or 
-    void SaveMaps(){
-
+    void SaveMaps()
+    {
+        string filePath = Path.Combine(Application.persistentDataPath, "maps.json");
+        string json = JsonUtility.ToJson(maps, true);
+        File.WriteAllText(filePath, json);
+        Debug.Log("Maps saved to: " + filePath);
     }
 }
