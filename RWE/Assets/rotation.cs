25d89ef315a9e1b6ea6507d96ef2e1cd3be953d1
Veeti <veeti.salminen@tuni.fi>
2024-03-14 18:51:22 +0200

    rotation and translation gain working like a wet dream

---
+++ RWE/Assets/rotation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+
+
+
+public class rotation : MonoBehaviour
+{
+
+    public GameObject gameobject;
+    public GameObject gazeinteractor;
+    private float change;
+    private float previousXRotation;
+    private float currentXRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousXRotation = gazeinteractor.transform.rotation.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameobject.transform.RotateAround(gazeinteractor.transform.position, Vector3.up, 25*Time.deltaTime);
+        Quaternion playerRotation = gazeinteractor.transform.rotation;
+        currentXRotation = playerRotation.eulerAngles.y;
+
+        change = currentXRotation - previousXRotation;
+
+        gameobject.transform.RotateAround(gazeinteractor.transform.position, Vector3.up, 1*change);
+
+        playerRotation = gazeinteractor.transform.rotation;
+        currentXRotation = playerRotation.eulerAngles.y;
+
+        previousXRotation = currentXRotation;
+    }
+}
