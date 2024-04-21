eab3c43dcd031aef8e09e3b8b780686aa32e9977
Veeti <veeti.salminen@tuni.fi>
2024-04-21 20:35:33 +0300

    added letter and anttis spaghetti

--- RWE/Assets/Scripts/Vector_manager.cs
+++ RWE/Assets/Scripts/Vector_manager.cs
@@ -40,6 +40,39 @@ public class Vector_manager : MonoBehaviour
         new Vector3(0f, 0f, -5.6f),
         new Vector3(3.962f, 0f, -3.962f)
     };
+    public static void ResetState(){
+        // Reset Curvature_points    
+        Curvature_points = new Vector3[]
+        {
+            new Vector3(5, 0, 0),        
+            new Vector3(10, 0, 0),
+            new Vector3(10, 0, 2),        
+            new Vector3(8, 0, 2),
+            new Vector3(10, 0, 10),        
+            new Vector3(10, 0, 15)
+        };
+        // Reset Rotation_points    
+        Rotation_points = new Vector3[]
+        {        new Vector3(4, 0, 0),
+            new Vector3(4, 0, 4),        
+            new Vector3(0, 0, 4),
+            new Vector3(0, 0, 0),        
+            new Vector3(4, 0, -4),
+            new Vector3(4, 0, -4),
+            new Vector3(0, 0, 0)    
+        };
+        // Reset Bending_points
+        Bending_points = new Vector3[]    {
+            new Vector3(5.6f, 0f, 0f),        
+            new Vector3(3.962f, 0f, 3.962f),
+            new Vector3(0f, 0f, 5.6f),        
+            new Vector3(-3.962f, 0f, 3.962f),
+            new Vector3(-5.6f, 0f, 0f),        
+            new Vector3(-3.962f, 0f, -3.962f),
+            new Vector3(0f, 0f, -5.6f),        
+            new Vector3(3.962f, 0f, -3.962f)
+        };
+    }
 }
 
 
