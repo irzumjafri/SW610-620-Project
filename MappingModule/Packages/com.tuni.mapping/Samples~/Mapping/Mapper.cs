7942b0e1548a8bf2fbdf40f9482a0c65013a7da1
Samu Toljamo <samu.toljamo@tuni.fi>
2024-05-14 15:17:56 +0300

    Final mapped

--- MappingModule/Packages/com.tuni.mapping/Samples~/Mapping/Mapper.cs
+++ MappingModule/Packages/com.tuni.mapping/Samples~/Mapping/Mapper.cs
@@ -1,11 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.InputSystem;
-using UnityEngine.XR.ARFoundation;
-using UnityEngine.UI;
-using TMPro;
 using System;
 using System.Threading.Tasks;
 using System.Linq;
